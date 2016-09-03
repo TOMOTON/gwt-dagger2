1d2b3cab5214823249b801578e1f560aaaaf0fad
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2016-09-03 13:37:24 +0200

    Apply code splitting and move all components and modules as CoffeeApp inner classes

--- src/main/java/coffee/client/CoffeeApp.java
+++ src/main/java/coffee/client/CoffeeApp.java
@@ -18,20 +18,61 @@
 package coffee.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.RunAsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+import dagger.Binds;
 import dagger.Component;
+import dagger.Module;
+import dagger.Provides;
+import dagger.Subcomponent;
+import java.util.function.Consumer;
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 public class CoffeeApp implements EntryPoint {
 
     @Override public void onModuleLoad() {
         Util.log("GWT+Dagger2 v" + System.getProperty("project.version") + " starting...");
-        Coffee coffee = DaggerCoffeeApp_Coffee.builder().build();
-        coffee.maker().brew();
+        Bootstrapper bootstrapper = DaggerCoffeeApp_Bootstrapper.builder().build();
+
+        Button drip = new Button("Drip Coffee");
+        drip.addClickHandler(e -> bootstrapper.dripCoffeeProxy().get(o -> o.maker().brew()));
+        RootPanel.get().add(drip);
     }
 
-    @Singleton
-    @Component(modules = { DripCoffeeModule.class })
-    public interface Coffee {
+    @Singleton @Component interface Bootstrapper {
+        DripCoffeeComponent dripCoffeeComponent(DripCoffeeModule dripCoffeeModule);
+        DripCoffeeProxy dripCoffeeProxy();
+
+        class DripCoffeeProxy {
+            @Inject Bootstrapper bootstrapper;
+            @Inject DripCoffeeProxy() {}
+            void get(Consumer<DripCoffeeComponent> fn) {
+                GWT.runAsync(new RunAsyncCallback() {
+                    @Override public void onFailure(Throwable reason) {
+                        GWT.log("error loading " + DripCoffeeComponent.class.getName(), reason);
+                    }
+                    @Override public void onSuccess() {
+                        fn.accept(bootstrapper.dripCoffeeComponent(new DripCoffeeModule()));
+                    }
+                });
+            }
+        }
+    }
+
+    @Singleton @Subcomponent(modules = DripCoffeeModule.class) interface DripCoffeeComponent {
         CoffeeMaker maker();
+    }
+
+    @Module(includes = PumpModule.class) static class DripCoffeeModule {
+        @Provides @Singleton Heater provideHeater() {
+            return new ElectricHeater();
+        }
+    }
+
+    @Module abstract static class PumpModule {
+        @Binds abstract Pump providePump(Thermosiphon pump);
     }
 }
